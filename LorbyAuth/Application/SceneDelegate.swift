b465cf7fc3bc9e6cda1805104e2918b90d1914ea
Anjella <anjellakattoobekova@gmail.com>
2024-03-08 02:32:10 +0600

    Customized the project by arichitecture and added views to the LunchScreen

---
+++ LorbyAuth/Application/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  LorbyAuth
+//
+//  Created by anjella on 29/2/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let onboardingViewController = ViewController()
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(rootViewController: onboardingViewController)
+    }
+}
+
